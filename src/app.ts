a62064bc369af81211e7d21972a5e79f8751cbbb
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-02-15 16:33:24 +0100

    use cursors to control logo

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,42 @@
 /// <reference path="../lib/phaser.d.ts"/>
 class SimpleGame {
+    game:Phaser.Game;
+    logo:Phaser.Sprite;
+    cursors:Phaser.CursorKeys;
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", {preload: this.preload, create: this.create});
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", {
+            preload: this.preload,
+            create: this.create,
+            update: this.update
+        });
     }
-
-    game:Phaser.Game;
 
     preload() {
         this.game.load.image("logo", "images/phaser-logo-small.png");
     }
 
     create() {
-        let logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-        logo.anchor.setTo(0.5, 0.5);
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
+        this.logo.anchor.setTo(0.5, 0.5);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.game.input.update();
+
+        if (this.cursors.down.isDown)
+            this.logo.position.y++;
+        if (this.cursors.up.isDown)
+            this.logo.position.y--;
+        if (this.cursors.left.isDown)
+            this.logo.position.x--;
+        if (this.cursors.right.isDown)
+            this.logo.position.x++;
     }
 
 }
 
 window.onload = () => {
-
     let game = new SimpleGame();
-
 };
